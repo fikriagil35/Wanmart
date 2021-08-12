4126d5a2f2f449da4cae3b8ba17883e05a952109
Rafi Priatna K <rafipriatnak@gmail.com>
2021-08-13 00:03:57 +0700

    Fix typo

--- application/views/user/pesan/index.php
+++ application/views/user/pesan/index.php
@@ -53,7 +53,7 @@
         <div class="modal-dialog">
             <div class="modal-content">
                 <div class="modal-header">
-                    <h5 class="modal-title">Hapus Data?</h5>
+                    <h5 class="modal-title">Tambah Pesan ke Admin</h5>
                     <button type="button" class="close" data-dismiss="modal" aria-label="Close">
                         <span aria-hidden="true">&times;</span>
                     </button>
